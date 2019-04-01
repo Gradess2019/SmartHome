0c58e21bfcd7aac680a0e34f9adb634457561ec8
Stepan <trofimovjava@gmail.com>
2019-04-02 00:38:32 +0300

    Restruct project

--- SmartHomeServer/src/com/gradesscompany/smarthome/Main.java
+++ SmartHomeServer/src/com/gradesscompany/smarthome/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("server_gui.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("fxml/server_gui.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
